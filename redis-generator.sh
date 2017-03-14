aa076234f9c2056459a258b1b5888e3bf44bc0bd
George Liu <eva2000@centminmod.com>
2017-03-15 07:16:40 +1000

    add some routine checks for setup

--- redis-generator.sh
+++ redis-generator.sh
@@ -44,19 +44,21 @@ genredis() {
         if [ ! -f "/etc/redis${REDISPORT}.conf" ]; then
           echo "create /etc/redis${REDISPORT}.conf config file"
           echo "cp -a /etc/redis.conf /etc/redis${REDISPORT}.conf"
-        fi      
-        echo "sed -i \"s|^port 6379|port $REDISPORT|\" "/etc/redis${REDISPORT}.conf""
-        echo "sed -i 's|^daemonize no|daemonize yes|' "/etc/redis${REDISPORT}.conf""
-        echo "sed -i \"s|pidfile \/var\/run\/redis_6379.pid|pidfile \/var\/run\/redis\/redis_${REDISPORT}.pid|\" "/etc/redis${REDISPORT}.conf""
-        echo "sed -i \"s|\/var\/log\/redis\/redis.log|\/var\/log\/redis\/redis${REDISPORT}.log|\" "/etc/redis${REDISPORT}.conf""
-        echo "sed -i \"s|dbfilename dump.rdb|dbfilename dump${REDISPORT}.rdb|\" "/etc/redis${REDISPORT}.conf""
-        echo "sed -i \"s|appendfilename \"appendonly.aof\"|appendfilename \"appendonly${REDISPORT}.aof\"|\" "/etc/redis${REDISPORT}.conf""
-        echo "echo \"#masterauth abc123\" >> "/etc/redis${REDISPORT}.conf""
-        echo "sed -i \"s|\/etc\/redis.conf|\/etc\/redis${REDISPORT}.conf|\" "/usr/lib/systemd/system/redis${REDISPORT}.service""
-        echo "systemctl daemon-reload"
-        echo "systemctl restart redis${REDISPORT}"
-        echo "Redis TCP $REDISPORT Info:"
-        echo "redis-cli -h 127.0.0.1 -p $REDISPORT INFO SERVER"
+        fi
+        if [[ -f "/etc/redis${REDISPORT}.conf" && ! "$(grep "dump${REDISPORT}.rdb" /etc/redis${REDISPORT}.conf)" ]] || [[ "$DEBUG_REDISGEN" = [yY] ]]; then
+          echo "sed -i \"s|^port 6379|port $REDISPORT|\" "/etc/redis${REDISPORT}.conf""
+          echo "sed -i 's|^daemonize no|daemonize yes|' "/etc/redis${REDISPORT}.conf""
+          echo "sed -i \"s|pidfile \/var\/run\/redis_6379.pid|pidfile \/var\/run\/redis\/redis_${REDISPORT}.pid|\" "/etc/redis${REDISPORT}.conf""
+          echo "sed -i \"s|\/var\/log\/redis\/redis.log|\/var\/log\/redis\/redis${REDISPORT}.log|\" "/etc/redis${REDISPORT}.conf""
+          echo "sed -i \"s|dbfilename dump.rdb|dbfilename dump${REDISPORT}.rdb|\" "/etc/redis${REDISPORT}.conf""
+          echo "sed -i \"s|appendfilename \"appendonly.aof\"|appendfilename \"appendonly${REDISPORT}.aof\"|\" "/etc/redis${REDISPORT}.conf""
+          echo "echo \"#masterauth abc123\" >> "/etc/redis${REDISPORT}.conf""
+          echo "sed -i \"s|\/etc\/redis.conf|\/etc\/redis${REDISPORT}.conf|\" "/usr/lib/systemd/system/redis${REDISPORT}.service""
+          echo "systemctl daemon-reload"
+          echo "systemctl restart redis${REDISPORT}"
+          echo "Redis TCP $REDISPORT Info:"
+          echo "redis-cli -h 127.0.0.1 -p $REDISPORT INFO SERVER"
+        fi
       else
         if [ ! -f "/usr/lib/systemd/system/redis${REDISPORT}.service" ]; then
           echo "create systemd redis${REDISPORT}.service"
@@ -68,19 +70,21 @@ genredis() {
           echo "cp -a /etc/redis.conf /etc/redis${REDISPORT}.conf"
           cp -a /etc/redis.conf "/etc/redis${REDISPORT}.conf"
         fi
-        ls -lah "/usr/lib/systemd/system/redis${REDISPORT}.service" "/etc/redis${REDISPORT}.conf "
-        sed -i "s|^port 6379|port $REDISPORT|" "/etc/redis${REDISPORT}.conf"
-        sed -i 's|^daemonize no|daemonize yes|' "/etc/redis${REDISPORT}.conf"
-        sed -i "s|pidfile \/var\/run\/redis_6379.pid|pidfile \/var\/run\/redis\/redis_${REDISPORT}.pid|" "/etc/redis${REDISPORT}.conf"
-        sed -i "s|\/var\/log\/redis\/redis.log|\/var\/log\/redis\/redis${REDISPORT}.log|" "/etc/redis${REDISPORT}.conf"
-        sed -i "s|dbfilename dump.rdb|dbfilename dump${REDISPORT}.rdb|" "/etc/redis${REDISPORT}.conf"
-        sed -i "s|appendfilename \"appendonly.aof\"|appendfilename \"appendonly${REDISPORT}.aof\"|" "/etc/redis${REDISPORT}.conf"
-        echo "#masterauth abc123" >> "/etc/redis${REDISPORT}.conf"
-        sed -i "s|\/etc\/redis.conf|\/etc\/redis${REDISPORT}.conf|" "/usr/lib/systemd/system/redis${REDISPORT}.service"
-        systemctl daemon-reload
-        systemctl restart redis${REDISPORT}
-        echo "## Redis TCP $REDISPORT Info ##"
-        redis-cli -h 127.0.0.1 -p $REDISPORT INFO SERVER
+        ls -lah "/usr/lib/systemd/system/redis${REDISPORT}.service" "/etc/redis${REDISPORT}.conf"
+        if [[ -f "/etc/redis${REDISPORT}.conf" && ! "$(grep "dump${REDISPORT}.rdb" /etc/redis${REDISPORT}.conf)" ]]; then
+          sed -i "s|^port 6379|port $REDISPORT|" "/etc/redis${REDISPORT}.conf"
+          sed -i 's|^daemonize no|daemonize yes|' "/etc/redis${REDISPORT}.conf"
+          sed -i "s|pidfile \/var\/run\/redis_6379.pid|pidfile \/var\/run\/redis\/redis_${REDISPORT}.pid|" "/etc/redis${REDISPORT}.conf"
+          sed -i "s|\/var\/log\/redis\/redis.log|\/var\/log\/redis\/redis${REDISPORT}.log|" "/etc/redis${REDISPORT}.conf"
+          sed -i "s|dbfilename dump.rdb|dbfilename dump${REDISPORT}.rdb|" "/etc/redis${REDISPORT}.conf"
+          sed -i "s|appendfilename \"appendonly.aof\"|appendfilename \"appendonly${REDISPORT}.aof\"|" "/etc/redis${REDISPORT}.conf"
+          echo "#masterauth abc123" >> "/etc/redis${REDISPORT}.conf"
+          sed -i "s|\/etc\/redis.conf|\/etc\/redis${REDISPORT}.conf|" "/usr/lib/systemd/system/redis${REDISPORT}.service"
+          systemctl daemon-reload
+          systemctl restart redis${REDISPORT}
+          echo "## Redis TCP $REDISPORT Info ##"
+          redis-cli -h 127.0.0.1 -p $REDISPORT INFO SERVER
+        fi
       fi
   done
 }
