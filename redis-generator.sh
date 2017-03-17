8357733055e81b25dc68a528789b51d86ecf4127
George Liu <eva2000@centminmod.com>
2017-03-18 07:43:40 +1000

    add sentinel to replication mode

--- redis-generator.sh
+++ redis-generator.sh
@@ -14,11 +14,16 @@ DEBUG_REDISGEN='y'
 UNIXSOCKET='n'
 SENTINEL_SETUP='n'
 INSTALLDIR='/svr-setup'
+SENTDIR='/root/tools/redis-sentinel'
 ######################################################
 # functions
 #############
 if [ ! -d "$INSTALLDIR" ]; then
   mkdir -p "$INSTALLDIR"
+fi
+
+if [ ! -d ${SENTDIR} ]; then
+  mkdir -p ${SENTDIR}
 fi
 
 if [ ! -f /usr/lib/systemd/system/redis.service ]; then
@@ -97,8 +102,8 @@ genredis_del() {
     if [ -f "/var/lib/redis/appendonly${REDISPORT}.aof" ]; then
       rm -rf "/var/lib/redis/appendonly${REDISPORT}.aof"
     fi
-    if [ -f "/root/tools/rediscluster/sentinel-${REDISPORT}.conf" ]; then
-      rm -rf "/root/tools/rediscluster/sentinel-${REDISPORT}.conf"
+    if [ -f "${SENTDIR}/sentinel-${REDISPORT}.conf" ]; then
+      rm -rf "${SENTDIR}/sentinel-${REDISPORT}.conf"
     fi
     if [ -d "/var/lib/redis/sentinel_${REDISPORT}" ]; then
       rm -rf "/var/lib/redis/sentinel_${REDISPORT}"
@@ -204,6 +209,68 @@ genredis() {
               echo "redis-cli -h 127.0.0.1 -p $REDISPORT INFO REPLICATION"
             fi
           fi
+          # sentinel setup for redis replication
+          if [[ "$SENTINEL_SETUP" && "$CLUSTER" = 'replication' && "$REDISPORT" = "$STARTPORT" ]]; then
+            SPORT="$STARTPORT"
+            SENTPORT=$((SPORT+10000))
+            echo
+            echo "-----------------"
+            echo "creating ${SENTDIR}/sentinel-${SPORT}.conf ..."
+            echo "mkdir -p "/var/lib/redis/sentinel_${SPORT}""
+            echo "create sentinel config: ${SENTDIR}/sentinel-${SPORT}.conf"
+            echo
+            echo "------------------------------------------"
+            echo "port $SENTPORT"
+            echo "daemonize yes"
+            echo "dir /var/lib/redis/sentinel_${SPORT}"
+            echo "pidfile /var/run/redis/redis-sentinel-${SPORT}.pid"
+            echo "sentinel monitor master-${SPORT} 127.0.0.1 ${SPORT} 1"
+            echo "sentinel down-after-milliseconds master-${SPORT} 3000"
+            echo "sentinel failover-timeout master-${SPORT} 6000"
+            echo "sentinel parallel-syncs master-${SPORT} 1"
+            echo "logfile /var/log/redis/sentinel-${SPORT}.log"
+            echo "------------------------------------------"
+            echo
+            echo "create startup scrip: "/etc/init.d/sentinel_$SENTPORT""
+            echo
+echo "
+#!/bin/bash
+# chkconfig: - 55 45
+# Start/Stop/restart script for Redis Sentinel
+
+NAME=\$(basename \${0})
+EXEC=/etc/redis${SPORT}/redis-server
+PIDFILE=/var/run/redis/redis-sentinel-${SPORT}.pid
+CONF=${SENTDIR}/sentinel-${SPORT}.conf
+
+PID=\$(cat \$PIDFILE 2> /dev/null)
+case "\$1" in
+     start)
+         echo \"Starting \$NAME ...\"
+         touch \$PIDFILE
+         exec \$EXEC \$CONF --sentinel --pidfile \$PIDFILE
+         ;;
+     stop)
+         echo \"Stopping \$NAME PID: \$PID ...\"
+         kill \$PID
+         ;;
+     restart)
+         echo \"Restarting \$NAME ...\"
+         \$0 stop
+         sleep 2
+         \$0 start
+         ;;
+     *)
+         echo \"Usage \$0 {start|stop|restart}\"
+         ;;
+esac
+"
+            echo
+            echo "chmod +x "/etc/init.d/sentinel_$SENTPORT""
+            echo "systemctl enable "sentinel_$SENTPORT""
+            echo "systemctl start "sentinel_$SENTPORT""
+            echo "tail -3 "/var/log/redis/sentinel-${SPORT}.log""
+          fi
         fi
       else
         if [ ! -f "/usr/lib/systemd/system/redis${REDISPORT}.service" ]; then
@@ -290,29 +357,70 @@ genredis() {
             fi
           fi
           # sentinel setup for redis replication
-          if [[ "$SENTINEL_SETUP" && "$CLUSTER" = 'replication' ]]; then
+          if [[ "$SENTINEL_SETUP" && "$CLUSTER" = 'replication' && "$REDISPORT" = "$STARTPORT" ]]; then
             SPORT="$STARTPORT"
             SENTPORT=$((SPORT+10000))
-            echo "creating /root/tools/rediscluster/sentinel-${SPORT}.conf ..."
+            echo
+            echo "-----------------"
+            echo "creating ${SENTDIR}/sentinel-${SPORT}.conf ..."
             mkdir -p "/var/lib/redis/sentinel_${SPORT}"
-cat > "/root/tools/rediscluster/sentinel-${SPORT}.conf" <<JJJ
-#port $SENTPORT
+cat > "${SENTDIR}/sentinel-${SPORT}.conf" <<JJJ
+port $SENTPORT
 daemonize yes
 dir /var/lib/redis/sentinel_${SPORT}
 pidfile /var/run/redis/redis-sentinel-${SPORT}.pid
-sentinel monitor master-${PORT} 127.0.0.1 ${SPORT} 1
+sentinel monitor master-${SPORT} 127.0.0.1 ${SPORT} 1
 sentinel down-after-milliseconds master-${SPORT} 3000
 sentinel failover-timeout master-${SPORT} 6000
 sentinel parallel-syncs master-${SPORT} 1
 logfile /var/log/redis/sentinel-${SPORT}.log
 JJJ
 
-            if [ -f "/root/tools/rediscluster/sentinel-${SPORT}.conf" ]; then
-              echo "sentinel (sentinel-${SPORT}.conf)"
-              cat "/root/tools/rediscluster/sentinel-${SPORT}.conf"
+            if [ -f "${SENTDIR}/sentinel-${SPORT}.conf" ]; then
+              echo "sentinel sentinel-${SPORT}.conf contents"
+              echo
+              cat "${SENTDIR}/sentinel-${SPORT}.conf"
             
               echo "starting Redis sentinel (sentinel-${SPORT}.conf)"
-              /etc/redis${SPORT}/redis-server "/root/tools/rediscluster/sentinel-${SPORT}.conf" --sentinel
+              # /etc/redis${SPORT}/redis-server "${SENTDIR}/sentinel-${SPORT}.conf" --sentinel
+
+cat > "/etc/init.d/sentinel_$SENTPORT" <<TTT
+#!/bin/bash
+# chkconfig: - 55 45
+# Start/Stop/restart script for Redis Sentinel
+
+NAME=\$(basename \${0})
+EXEC=/etc/redis${SPORT}/redis-server
+PIDFILE=/var/run/redis/redis-sentinel-${SPORT}.pid
+CONF=${SENTDIR}/sentinel-${SPORT}.conf
+
+PID=\$(cat \$PIDFILE 2> /dev/null)
+case "\$1" in
+     start)
+         echo "Starting \$NAME ..."
+         touch \$PIDFILE
+         exec \$EXEC \$CONF --sentinel --pidfile \$PIDFILE
+         ;;
+     stop)
+         echo "Stopping \$NAME PID: \$PID ..."
+         kill \$PID
+         ;;
+     restart)
+         echo "Restarting \$NAME ..."
+         \$0 stop
+         sleep 2
+         \$0 start
+         ;;
+     *)
+         echo "Usage \$0 {start|stop|restart}"
+         ;;
+esac
+TTT
+            chmod +x "/etc/init.d/sentinel_$SENTPORT"
+            systemctl enable "sentinel_$SENTPORT"
+            systemctl start "sentinel_$SENTPORT"
+            tail -3 "/var/log/redis/sentinel-${SPORT}.log"
+
             fi
           fi
         fi
